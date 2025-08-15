73f836b71c5157487652a17719291b4bcda3f128
SrihariWebDeveloper <146625393+SrihariWebDeveloper@users.noreply.github.com>
2025-08-15 22:48:45 +0530

    Update Login.jsx

--- Admin/src/Components/AdminAuth/Login.jsx
+++ Admin/src/Components/AdminAuth/Login.jsx
@@ -11,7 +11,7 @@ const Login = ({setToken}) => {
     const onSubmitHandler = async(e)=>{
       try {
         e.preventDefault();
-        const response = await axios.post("http://localhost:3000/api/user/admin",{email,password});
+        const response = await axios.post("https://shopwave-97x5.onrender.com/api/user/admin",{email,password});
         if(response.data.success){
           setToken(response.data.token);
         }else{
